7a2e4bafe61946f455670cd18a4faf10deeddfb3
dd <ddresearch@shaw.ca>
2017-04-25 13:31:55 -0700

    first commit

---
+++ AwesomeProject/index.ios.js
@@ -0,0 +1,84 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'https://i.imgur.com/UePbdph.jpg'}},
+];
+
+export default class AwesomeProject extends Component {
+  render() {
+    var movie = MOCKED_MOVIES_DATA[0];
+
+    return (
+      <View style={styles.container}>
+        <Image source={{uri: movie.posters.thumbnail}} 
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text> 
+        </View>
+      </View>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  }
+});
+
+/*
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+*/
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
---
+++ README.md
@@ -0,0 +1,11 @@
+# React Native Tutorial
+
+##  Description
+
+Tutorial to help familiarize myself with React Native.  Includes a tabular UI with real data calls to retrieve some sample movie data
+
+##  Source Material
+
+All source material is from Facebook.
+URL: [http://facebook.github.io/react-native/releases/0.25/docs/tutorial.html](http://facebook.github.io/react-native/releases/0.25/docs/tutorial.html)
+  
