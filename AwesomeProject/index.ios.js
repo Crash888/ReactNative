0e6c573962fcecf210c90c8c1c35c6d11b03ef51
dd <ddresearch@shaw.ca>
2017-04-25 22:27:59 -0700

    fetch data

--- AwesomeProject/index.ios.js
+++ AwesomeProject/index.ios.js
@@ -17,10 +17,52 @@ var MOCKED_MOVIES_DATA = [
   {title: 'Title', year: '2015', posters: {thumbnail: 'https://i.imgur.com/UePbdph.jpg'}},
 ];
 
-export default class AwesomeProject extends Component {
-  render() {
-    var movie = MOCKED_MOVIES_DATA[0];
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
+export default class AwesomeProject extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: null,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState ({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  }
+
+  render() {
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  }
+
+  renderMovie(movie) {
     return (
       <View style={styles.container}>
         <Image source={{uri: movie.posters.thumbnail}} 
